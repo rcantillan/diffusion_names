d3aa7c71cf60129e0c43a4c03f403915518a3fd5
Roberto Cantillan Carrasco <ricantillan@uc.cl>
2023-05-06 18:27:44 -0400

    local

--- explore_data.R
+++ explore_data.R
@@ -1,11 +1,20 @@
 
+library(ipumsr)
 library(readr)
+library(tidyr)
+library(dplyr)
+library(purrr)
+
+# setwd
 setwd("")
 Sys.setlocale( 'LC_ALL','C' ) 
 names<-read_delim("datos_1920a2021.txt", delim = ";", locale=locale(encoding="latin1")) 
 
+# separate year/month column 
+colnames(names)<-c("ano","comuna","nombre","cantidad")
+names<-separate(names, ano, into = c("ano","mes"), sep = c(4))
+
 # descr
 com<-names$COMUNA
 lapply(com,agrep,com,value=TRUE)
-
 
