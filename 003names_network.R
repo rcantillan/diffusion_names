8f2c4d63b3298995996c61b32ec4bd885b4e1d0f
Roberto Cantillan Carrasco <ricantillan@uc.cl>
2023-05-22 01:56:55 -0400

    local

--- 003names_network.R
+++ 003names_network.R
@@ -67,12 +67,12 @@ g %>%
                  alpha = 0.8, strength = 0.2, show.legend = FALSE) +
   scale_edge_width_continuous(range = c(0.1,2)) +
   scale_edge_colour_identity() +
-  geom_node_point(aes(x=x, y=y, size = betweenness, fill = "darkblue"), colour = "white", 
+  geom_node_point(aes(x=x, y=y, size = NULL, fill = "darkblue"), colour = "white", 
                   pch = 21, alpha = 0.7, show.legend = FALSE) +
   scale_size_continuous(range = c(1,8)) +
   scale_fill_identity() +
   ggnewscale::new_scale("size") + 
-  geom_node_text(aes(label = name, filter = degree >= 15), colour = "#000000", 
+  geom_node_text(aes(label = name, filter = NULL), colour = "#000000", 
                  size = 3, family = "calibri", repel = T) + 
   scale_size_continuous(range = c(0.5,5)) +
   theme_void()
