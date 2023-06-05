25b0d228f56d082c75a303880d66427860c3b8f1
Roberto Cantillan Carrasco <ricantillan@uc.cl>
2023-06-05 00:45:36 -0400

    local

--- code/001explore_data.R
+++ code/001explore_data.R
@@ -9,6 +9,7 @@ library(migraph)
 library(stringi)
 library(ggraph)
 library(ggforce)
+library(tidytext)
 library(graphlayouts)
 library(stringdist)
 library(bipartite)
@@ -64,7 +65,7 @@ comunas_x<-names_sample %>%
 #Por ejemplo, 
 
 # 1. Cuantos nombres nuevos hay por ano? 
-# 2. Cuan comun son ciertos nombres, es decir, en que #proporcion de comunas aparecen los nombres por comuna? 
+# 2. Cuan comun son ciertos nombres, es decir, en que proporcion de comunas aparecen los nombres por comuna? 
 # 3. Hay alguna division territorial en como la gente da nombres a sus hijos? Hay alguna diferencia entre el sur y el norte (sur/norte de Stgo)? 
 
 #Este tipo de informacion nos puede ayudar a darnos una idea de como va a ser la red de nombres.
@@ -121,9 +122,143 @@ result %>%
   ggplot(aes(x = ano, y = new_names)) +
   geom_point()+
   geom_smooth(method = "loess") + 
-  xlim(1921, 2022) +
+  xlim(1921, 2021) +
   stat_cor(method = "pearson", 
-           label.x = 2000, label.y = 1100) 
+           label.x = 2000, label.y = 1100) +
+  labs(y = "New names per year (n)", x = "Year")
+  
+
+
+# Assuming your list of data frames is named "df_list"
+# Use map to iterate over each data frame in the list
+result_list <- map(names_samples, ~ {
+  # Group by "name" and "comuna" and count occurrences
+  count_df <- count(.x, nombre, comuna) %>%
+    arrange(desc(n))
+  # Select the top 100 most common names
+  top_600 <- count_df %>%
+    group_by(nombre) %>%
+    top_n(1, wt = n) %>%
+    ungroup() %>%
+    top_n(600, wt = n) %>%
+    arrange(desc(n))
+  # Return the resulting data frame
+  top_300
+})
+
+# Combine the resulting data frames into one
+combined_df <- bind_rows(result_list)
+
+# Rename the count column to "amount"
+combined_df <- rename(combined_df, amount = n)
+
+
+# Combine "name" and "comuna" from all data frames into a single data frame
+combined_df <- bind_rows(names_samples, .id = "df_id") %>%
+  select(df_id, ano, comuna, nombre) %>%
+  count(nombre, comuna, sort = TRUE)
+
+top_names <- combined_df %>%
+  top_n(600, n) %>%
+  arrange(desc(n))
+
+# Customize the size of the nodes based on the frequency
+node_sizes <- top_names %>%
+  mutate(size = n/600) # Adjust the scaling factor as needed
+
+# Plot the network graph with differentiated node sizes
+graph <- ggplot() +
+  geom_node_point(aes(x = comuna, y = nombre, size = size, color = size), data = node_sizes) +
+  #scale_fill_viridis_c(guide = "legend") +
+  scale_color_viridis(option = "G",   direction = 1) +
+  scale_size_continuous(range = c(0, 6)) +
+  labs(x = "Commune", y = "Name") +
+  #theme_minimal() +
+  guides(size = "legend", colour = "legend") +
+  theme(axis.ticks.y=element_blank(),
+        #legend.position = "top",
+        panel.grid.major.y=element_blank(),
+        plot.title = element_text(hjust = 0.5, size = 8),
+        axis.title = element_text(size=10),
+        axis.text.x = element_text(size = 7,angle = 90, hjust = 0.9),
+        axis.text.y = element_text(size = 7))
+
+# Display the graph
+print(graph)
+
+
+# network
+nodes1<-node_sizes%>%select(nombre)
+nodes1$type<-"nombre"
+colnames(nodes1)<-c("id","type")
+nodes2<-node_sizes%>%select(comuna)
+nodes2$type<-"comuna"
+colnames(nodes2)<-c("id","type")
+attr<-rbind(nodes1,nodes2)
+attr<-unique(attr)
+
+g <- graph_from_data_frame(d=node_sizes, vertices = attr)
+
+
+# Customize the appearance of the nodes and edges
+ggraph(g, layout = "kk") +
+  geom_edge_link0(aes(alpha = size, width = size, colour = n), 
+                  arrow = arrow(angle = 30,length = unit(0.15, "inches"), ends = "last", type = "closed")) + 
+  scale_edge_colour_gradient(low = "#87CEFF", high = "#27408B") + 
+ # geom_edge_arc2(aes(alpha = size, width = size, colour = n), 
+ #               arrow = arrow(angle = 30,length = unit(0.15, "inches"), ends = "last", type = "closed")) +
+  scale_edge_width(range = c(0.3, 1.8)) + 
+  scale_edge_alpha(range = c(0.1, 1)) + 
+  geom_node_point(aes(fill = type, size = all_degree),
+                  colour = "#000000", shape = 21, stroke = 0.3) + 
+  scale_fill_brewer(palette = "Set1", na.value = "gray53") + 
+  scale_size(range = c(3,8)) + 
+  #geom_node_text(aes(label = name), colour = "#000000", size = 3.5, family = "sans") + 
+  geom_node_label(aes(label = name), repel = TRUE, size = 2.5) +
+  #theme_graph() + 
+  theme(legend.position = "none")
+  #geom_node_text(aes(label = name), repel = TRUE) +
+  theme_void()
+
+# Display the result and the network map
+print(result)
+
+
+# network. 
+node_sizes%>%
+  filter(comuna%in%c("ANTOFAGASTA","SANTIAGO","PUERTO MONTT"))%>%
+ggplot(aes(x=reorder_within(nombre, n, comuna), y=n, fill=comuna)) +
+  geom_col(show.legend = FALSE) +
+  scale_fill_viridis_d(option = "D")+
+  facet_wrap(~comuna, ncol = 1, scales = "free_y") +
+  coord_flip() +
+  scale_x_reordered() +
+  scale_y_continuous(expand = c(0,0)) +
+  labs(title = "", y = "", x=NULL) +
+  theme_gray(base_size = 7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
